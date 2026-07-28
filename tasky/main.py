e0cb823d14f50a950b6dc39d11503352888b9f50
Chiazokam Echeta <chiazokamecheta@gmail.com>
2026-07-28 17:41:56 +0100

    feat: read endpoints with 404

--- tasky/main.py
+++ tasky/main.py
@@ -2,6 +2,23 @@ from fastapi import FastAPI
 
 app = FastAPI(title="Tasky")
 
+tasks = [
+    {
+        "id": 1,
+        "title": "Task 1",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Task 2",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Task 3",
+        "done": True
+    }
+]
 
 @app.get("/")
 async def root():
@@ -10,3 +27,15 @@ async def root():
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    task = next((task for task in tasks if task["id"] == id), None)
+    if task:
+        return task
+    else:
+        return  { "error": f"Task {id} not found" }, 404
